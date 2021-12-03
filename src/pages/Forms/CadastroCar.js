14a8dc7ba54143c2a3ab400263eed2c8ddff2b44
Luiz Henrique Santos <luiz.santos85@gmail.com>
2021-12-03 16:37:01 -0300

    correçao de erros de upload de fotos

--- src/pages/Forms/CadastroCar.js
+++ src/pages/Forms/CadastroCar.js
@@ -1,52 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './styled.module.scss';
 import { useHistory } from 'react-router-dom';
 import useApi from '../../service/Api';
 import Loading from '../../components/Loading';
 
 const CadastroCar = () => {
-    const api = useApi();
-    const history = useHistory();
+   const api = useApi();
+   const history = useHistory();
 
-    const [loading, setLoading] = useState(true);
+   const [loading, setLoading] = useState(true);
+   const [marca, setMarca] = useState('');
+   const [modelo, setModelo] = useState('');
+   const [ano, setAno] = useState('');
+   const [preco, setPreco] = useState('');
+   const [photo, setPhoto] = useState('');
+    const photoField = useRef();
+   const [error, setError] = useState([]);
 
-    useEffect(() => {
-        const chekLogin = async () => {
-            if (api.getToken()) {
-                const result = await api.validateToken();
-                if (result.error === '') {
-                    setLoading(false);
-                } else {
-                    alert(result.error);
-                    window.location.href = '/login'; //dar o refresh na página
-                    localStorage.removeItem('token');
-                }
+   const handleFormSubmit = async (e) => {
+      e.preventDefault();
+      setError('');
+      let errors = [];
+      if (!marca.trim() || !modelo.trim() || !ano.trim() || !preco.trim()) {
+         errors.push('Preencha todos os campos');
+      }
+
+      if (errors.length === 0) {
+         let body = new FormData();
+         body.append('brand',marca);
+         body.append('name', modelo);
+         body.append('year', ano);
+         body.append('price', preco);
+
+         if(photoField.current.files.length > 0){
+            body.append('photo',photoField.current.files[0]);
+         }
+
+         const json = await api.addCar(body);
+
+         if (!json.error) {
+            history.push('/');
+            return;
+         } else {
+            setError(json.error);
+         }
+      } else {
+         setError(errors);
+      }
+   };
+
+   useEffect(() => {
+      const chekLogin = async () => {
+         if (api.getToken()) {
+            const result = await api.validateToken();
+            if (result.error === '') {
+               setLoading(false);
             } else {
-                history.push('/login');
+               alert(result.error);
+               window.location.href = '/login'; //dar o refresh na página
+               localStorage.removeItem('token');
             }
-        };
-        chekLogin();
-    }, []);
+         } else {
+            history.push('/login');
+         }
+      };
+      chekLogin();
+   }, []);
 
-    return (
-       <div className={`${styles.formGeral} animeLeft`}>
-          {loading && <Loading />}
-          {!loading && (
-             <>
-                <h2>Cadastrar Carro</h2>
-                <form >
-                   <input type="text" placeholder="Marca" />
-                   <input type="text" placeholder="Modelo" />
-                   <input type="text" placeholder="Ano" />
-                   <input type="text" placeholder="Preço" />
-                   <input type="file" />
+   return (
+      <div className={`${styles.formGeral} animeLeft`}>
+         {loading && <Loading />}
+         {!loading && (
+            <>
+               <h2>Cadastrar Carro</h2>
+               <form onSubmit={handleFormSubmit}>
+                  {error && <div className={styles.error}>{error}</div>}
+                  <input
+                     type="text"
+                     placeholder="Marca"
+                     disabled={loading}
+                     value={marca}
+                     onChange={(e) => setMarca(e.target.value)}
+                  />
+                  <input
+                     type="text"
+                     placeholder="Modelo"
+                     disabled={loading}
+                     value={modelo}
+                     onChange={(e) => setModelo(e.target.value)}
+                  />
+                  <input
+                     type="number"
+                     placeholder="Ano"
+                     disabled={loading}
+                     value={ano}
+                     onChange={(e) => setAno(e.target.value)}
+                  />
+                  <input
+                     type="number"
+                     placeholder="Preço"
+                     disabled={loading}
+                     value={preco}
+                     onChange={(e) => setPreco(e.target.value)}
+                  />
+                  <input
+                     type="file"
+                     ref={photoField}
+                     disabled={loading}
+                  />
 
-                   <button>Cadastrar carro</button>
-                </form>
-             </>
-          )}
-       </div>
-    );
+                  <button disabled={loading}>
+                     {loading ? 'Carregando...' : 'Cadastrar carro'}
+                  </button>
+               </form>
+            </>
+         )}
+      </div>
+   );
 };
 
 export default CadastroCar;
--- src/service/Api.js
+++ src/service/Api.js
@@ -1,46 +1,61 @@
-const baseUrl = 'https://apilaravel.lhscode.com.br/public/api';
+// const baseUrl = 'https://apilaravel.lhscode.com.br/public/api';
+const baseUrl = 'http://localhost:8000/api';
 
 const request = async (method, endpoint, params, token = null) => {
-    method = method.toLowerCase();
-    let fullUrl = `${baseUrl}${endpoint}`;
-    let body = null;
-    switch (method) {
-        case 'get':
-            let queryString = new URLSearchParams(params).toString();
-            fullUrl += `?${queryString}`;
-            break;
-        case 'post':
-        case 'put':
-        case 'delete':
-            body = JSON.stringify(params);
-            break;
-    }
-    let headers = {'Content-Type': 'application/json'};
-    if(token){
-        headers.Authorization = `Bearer ${token}`;
-    }
-    let req = await fetch(fullUrl,{method,headers,body});
-    let json = await req.json();
-    return json;
+   method = method.toLowerCase();
+   let fullUrl = `${baseUrl}${endpoint}`;
+   let body = null;
+   switch (method) {
+      case 'get':
+         let queryString = new URLSearchParams(params).toString();
+         fullUrl += `?${queryString}`;
+         break;
+      case 'post':
+      case 'put':
+      case 'delete':
+         body = JSON.stringify(params);
+         break;
+   }
+   let headers = { 'Content-Type': 'application/json' };
+   if (token) {
+      headers.Authorization = `Bearer ${token}`;
+   }
+   let req = await fetch(fullUrl, { method, headers, body });
+   let json = await req.json();
+   return json;
 };
 
 export default () => {
-    return {
-        getToken: () => {
-            return localStorage.getItem('token');
-        },
-        validateToken: async () =>{
-            let token = localStorage.getItem('token');
-            let json = await request('post','/auth/validate',{},token);
-            return json;
-        },
-        login: async (email, password) => {
-            let json = await request('post','/auth/login',{email,password});
-            return json;
-        },
-        getAllCars: async () =>{
-            let json = await request('get','/cars',{});
-            return json;
-        }
-    };
+   return {
+      getToken: () => {
+         return localStorage.getItem('token');
+      },
+      validateToken: async () => {
+         let token = localStorage.getItem('token');
+         let json = await request('post', '/auth/validate', {}, token);
+         return json;
+      },
+      login: async (email, password) => {
+         let json = await request('post', '/auth/login', { email, password });
+         return json;
+      },
+      getAllCars: async () => {
+         let token = localStorage.getItem('token');
+         let json = await request('get', '/cars', {}, token);
+         return json;
+      },
+      addCar: async (data) => {
+         let token = localStorage.getItem('token');
+         let req = await fetch(`${baseUrl}/car`,{
+            method:'POST',
+            headers:{
+               Authorization: `Bearer ${token}`
+            },
+            body: data
+         });
+         let json = await req.json();
+         return json;
+      },
+      updateCar: async (fData, id) => {},
+   };
 };
